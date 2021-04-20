69ccfe639aedf6294c54d4172998a203c53a106a
moonhopark <moonho4199@naver.com>
2021-04-20 23:06:17 +0900

    [stpe2] 아날로그 시계 및 정렬

--- 2nd_seminar/Clock-weather-app/clock.js
+++ 2nd_seminar/Clock-weather-app/clock.js
@@ -1,6 +1,9 @@
 const digitalTime = document.querySelector(".digital_time"),
   digitalButton = document.querySelector(".digital_button"),
-  header = document.querySelector(".header");
+  header = document.querySelector(".header"),
+  analogHour = document.querySelector(".analog_hour"),
+  analogMinute = document.querySelector(".analog_minute"),
+  analogSecond = document.querySelector(".analog_second");
 
 const fillZero = (num) => {
   num = num + "";
@@ -10,18 +13,15 @@ const fillZero = (num) => {
   } else {
     return num;
   }
-}
-
+};
 const getTime = () => {
   const now = new Date();
-
   let hour = now.getHours(),
     month = now.getMonth();
   const minute = now.getMinutes(),
     second = now.getSeconds(),
     year = now.getFullYear(),
     day = now.getDate();
-
   const monthList = [
     "January",
     "February",
@@ -37,16 +37,24 @@ const getTime = () => {
     "December",
   ];
   month = monthList[month];
-
-  if (hour >= 0 && hour <=11) {
+  if (hour >= 0 && hour <= 11) {
     if (hour === 0) hour = 12;
     digitalButton.innerHTML = "AM";
   } else {
-    if(hour >= 13 ) hour -= 12;
+    if (hour >= 13) hour -= 12;
     digitalButton.innerHTML = "PM";
   }
-
   return { year, month, day, hour, minute, second };
+};
+
+const drawClock = (hour, minute, second) => {
+  const hourDegree = (hour + minute / 60) * (360 / 12) + 90,
+    minuteDegree = (minute + second / 60) * (360 / 60) + 90,
+    secondDegree = second * (360 / 60) + 90;
+
+  analogHour.style.transform = `rotate(${hourDegree}deg)`;
+  analogMinute.style.transform = `rotate(${minuteDegree}deg)`;
+  analogSecond.style.transform = `rotate(${secondDegree}deg)`;
 };
 
 const drawTime = () => {
@@ -54,10 +62,11 @@ const drawTime = () => {
 
   header.innerHTML = `Today is <span>${day} ${month}</span>, ${year}`;
   digitalTime.innerHTML = `${fillZero(hour)} : ${fillZero(minute)} : ${fillZero(second)}`;
+
+  drawClock(hour, minute, second);
 };
 
 const init = () => {
   setInterval(drawTime, 1000);
 };
-
 init();
